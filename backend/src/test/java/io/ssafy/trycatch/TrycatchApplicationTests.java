6c077c510e136d1409fa473df7ab472bf190435f
정희수 <huissppea@gmail.com>
2026-01-20 14:23:51 +0900

    [S14P11E206-123] chore(be): backend setup

---
+++ backend/src/test/java/io/ssafy/trycatch/TrycatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ssafy.trycatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrycatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
