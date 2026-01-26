cab5d411955514b3455015936096319ae2594a41
박정원 <wjd1278@naver.com>
2026-01-26 12:41:14 +0900

    [S14P11E206-324] feat(fe) : 로그 데이터 수신 처리

--- frontend/src/api/questFile.ts
+++ frontend/src/api/questFile.ts
@@ -2,6 +2,7 @@
 
 import type { QuestInfo } from '../pages/game/types/ideTypes';
 
+// 로그 데이터 + 문제 파일 목록
 export const getQuest = async (questId: number): Promise<QuestInfo> => {
   // const response = await axios.get(
   //   `/api/v1/rooms/{roomId}/quest/{questId}/files`,
