aa98a3b61a7cd62cf1cc7330843b354df728c2ec
JS <kookyss121@naver.com>
2026-01-26 10:29:36 +0900

    [s14p11e206-200] feat(fe): 메인페이지 헤더에 로그인 라우터 연결

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,14 @@ import { Route, Routes } from 'react-router-dom';
 import MainLayout from './layouts/AppLayout';
 import HomePage from './pages/home/HomePage';
 import Idepage from './pages/ide/Idepage';
+import LoginPage from './pages/login/LoginPage';
 
 function App() {
   return (
     <Routes>
       <Route element={<MainLayout />}>
         <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/ide" element={<Idepage />} />
       </Route>
     </Routes>
