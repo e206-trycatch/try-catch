dedfb2f4c64ecca89747caf626ad31b06f17200d
박정원 <wjd1278@naver.com>
2026-01-27 12:01:04 +0900

    [S14P11E206-331][S14P11E206-333] feat(fe) : 제출 요청 및 실패 처리

--- frontend/src/pages/game/GamePage.tsx
+++ frontend/src/pages/game/GamePage.tsx
@@ -1,15 +1,19 @@
 import { Resizable } from 're-resizable';
 import { useEffect, useMemo, useState } from 'react';
 
+import { codeSubmission } from '../../api/codeSubmission';
+import { buildFilesRequestData } from '../../api/codeSubmissionMapper';
 import { getQuest } from '../../api/questFile';
 import CodeEditor from './components/CodeEditor';
 import Explorer from './components/Explorer';
 import FileTabs from './components/FileTabs';
 import MenuBar from './components/MenuBar';
+import SubmitBtn from './components/SubmitBtn';
 import Terminal from './components/Terminal';
 import { useFile } from './hooks/useFile';
 import { useIde } from './hooks/useIde';
 import useTerminal from './hooks/useTerminal';
+import type { SubmissionRequest } from './types/apiTypes';
 import type { QuestInfo } from './types/ideTypes';
 import type { FileNode } from './types/ideTypes';
 
@@ -21,6 +25,38 @@ export default function GamePage() {
   const [error, setError] = useState<string | null>(null);
 
   const [activeMenu, setActiveMenu] = useState<SideMenu>('explorer');
+
+  const submitCode = async () => {
+    const setRoomId = 1; // 임시 데이터
+    const frontFrameworkId = 1; // 임시 데이터
+    const backFrameworkId = 1; // 임시 데이터
+    const accessToken = '';
+    const requestBody: SubmissionRequest = {
+      frontend: {
+        problemFrameworkId: frontFrameworkId,
+        files: buildFilesRequestData({
+          node: rootNode,
+          fileCodes: ide.fileCodes,
+          role: 'FRONTEND',
+        }),
+      },
+      backend: {
+        problemFrameworkId: backFrameworkId,
+        files: buildFilesRequestData({
+          node: rootNode,
+          fileCodes: ide.fileCodes,
+          role: 'BACKEND',
+        }),
+      },
+    };
+    try {
+      const result = await codeSubmission(setRoomId, requestBody, accessToken);
+      console.log('제출 성공');
+      console.log(result);
+    } catch (e) {
+      console.error('제출 실패', e);
+    }
+  };
 
   useEffect(() => {
     const loadQuest = async () => {
@@ -67,65 +103,66 @@ export default function GamePage() {
   }
 
   return (
-    <div className=" flex w-full px-20 pt-[80px] pb-[40px] h-screen ">
-      {/* 메뉴바 */}
-      <div className="w-[70px] h-full bg-stone-900 py-5 px-2 border border-gray-700">
-        <MenuBar activeMenu={activeMenu} onChangeMenu={setActiveMenu} />
-      </div>
-
-      {/* 파일탐색기 + 코드 편집기 + 터미널 */}
-      <div className="flex flex-col flex-1 min-h-0 overflow-hidden">
-        {/* 파일 탐색기 + 코드 편집기 */}
-        <div className="flex border border-gray-700 flex-1 min-h-0">
-          <Resizable
-            defaultSize={{ width: 300, height: '100%' }}
-            minWidth={50}
-            maxWidth={400}
-            enable={{ right: true }} // 드래그 설정 - 오른쪽만
-            className="bg-stone-900 border-r border-gray-700"
-          >
-            <div className="h-full overflow-hidden p-5">
-              {/* 조건 && 컴포넌트 : 조건이 true일 때만 컴포넌트를 렌더링 */}
-              {activeMenu === 'explorer' && (
-                <Explorer
-                  root={rootNode}
-                  expanded={ide.expanded}
-                  onToggleFolder={ide.toggleFolder}
-                  onOpenFile={ide.openFile}
-                />
-              )}
-            </div>
-          </Resizable>
-
-          <div className="flex flex-col flex-1 min-w-0 min-h-0 ">
-            <FileTabs
-              openTabs={ide.openTabs}
-              activeFileId={ide.activeFileId}
-              onSelectTab={ide.selectTab}
-              onCloseTab={ide.closeTab}
-            />
-            <div className="flex-1 min-h-0 bg-[#1E1E1E]">
-              <CodeEditor
-                activeFile={ide.activeFile}
-                code={ide.currentCode}
-                onChange={ide.setCurrentCode}
+    <div className="w-full px-20 pt-[80px] pb-[40px] h-screen">
+      <div className=" flex w-full h-full">
+        {/* 메뉴바 */}
+        <div className="w-[70px] h-full bg-stone-900 py-5 px-2 border border-gray-700">
+          <MenuBar activeMenu={activeMenu} onChangeMenu={setActiveMenu} />
+        </div>
+        {/* 파일탐색기 + 코드 편집기 + 터미널 */}
+        <div className="flex flex-col flex-1 min-h-0 overflow-hidden">
+          {/* 파일 탐색기 + 코드 편집기 */}
+          <div className="flex border border-gray-700 flex-1 min-h-0">
+            <Resizable
+              defaultSize={{ width: 300, height: '100%' }}
+              minWidth={50}
+              maxWidth={400}
+              enable={{ right: true }} // 드래그 설정 - 오른쪽만
+              className="bg-stone-900 border-r border-gray-700"
+            >
+              <div className="h-full overflow-hidden p-5">
+                {/* 조건 && 컴포넌트 : 조건이 true일 때만 컴포넌트를 렌더링 */}
+                {activeMenu === 'explorer' && (
+                  <Explorer
+                    root={rootNode}
+                    expanded={ide.expanded}
+                    onToggleFolder={ide.toggleFolder}
+                    onOpenFile={ide.openFile}
+                  />
+                )}
+              </div>
+            </Resizable>
+            <div className="flex flex-col flex-1 min-w-0 min-h-0 ">
+              <FileTabs
+                openTabs={ide.openTabs}
+                activeFileId={ide.activeFileId}
+                onSelectTab={ide.selectTab}
+                onCloseTab={ide.closeTab}
               />
+              <div className="flex-1 min-h-0 bg-[#1E1E1E]">
+                <CodeEditor
+                  activeFile={ide.activeFile}
+                  code={ide.currentCode}
+                  onChange={ide.setCurrentCode}
+                />
+              </div>
             </div>
           </div>
+          {/* 터미널 */}
+          <Resizable
+            defaultSize={{ width: '100%', height: 220 }}
+            enable={{ top: true }}
+            className="shrink-0 border border-gray-700 overflow-hidden"
+            minHeight={50}
+          >
+            <Terminal
+              frontendErrorLog={frontendErrorLog}
+              backendErrorLog={backendErrorLog}
+            />
+          </Resizable>
         </div>
-        {/* 터미널 */}
-        <Resizable
-          defaultSize={{ width: '100%', height: 220 }}
-          enable={{ top: true }}
-          className="shrink-0 border border-gray-700 overflow-hidden"
-          minHeight={50}
-        >
-          <Terminal
-            frontendErrorLog={frontendErrorLog}
-            backendErrorLog={backendErrorLog}
-          />
-        </Resizable>
       </div>
+      <SubmitBtn onClick={submitCode} />
     </div>
   );
 }
--- frontend/src/pages/game/hooks/useIde.ts
+++ frontend/src/pages/game/hooks/useIde.ts
@@ -144,6 +144,7 @@ export function useIde(root: FileNode) {
   return {
     expanded,
     toggleFolder,
+    fileCodes,
 
     openTabs,
     activeFileId,
