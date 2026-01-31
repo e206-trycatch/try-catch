54e7d8a7399e85406b4291b0cb45858c1f1eaeec
우주영 <dlsrtyex63@gmail.com>
2026-01-31 17:33:53 +0900

    [S14P11E206-583] fix(be) : WS config - 요청 엔드포인트, origin 수정

--- backend/src/main/java/io/ssafy/trycatch/websocket/config/WebSocketConfig.java
+++ backend/src/main/java/io/ssafy/trycatch/websocket/config/WebSocketConfig.java
@@ -21,15 +21,16 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         // 클라이언트가 구독할 prefix
         registry.enableSimpleBroker("/topic");
         // 클라이언트 → 서버 메시지 보낼 때 prefix
-        // 클라이언트가 /app/chat 으로 보내면
-        // -> @MessageMapping("/chat") 핸들러가 받음
+        // 클라이언트가 /app/chat 으로 보내면 -> @MessageMapping("/chat") 핸들러가 받음
         registry.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws") //   ws://localhost:8081/ws
-                .setAllowedOriginPatterns("*")
+        registry.addEndpoint("api/ws") //   ws://localhost:8081/ws
+                .setAllowedOriginPatterns(
+                        "http://localhost:5173",
+                        "https://i14e206.p.ssafy.io" )
                 .withSockJS();  //WebSocket 미지원 브라우저 대비 폴백
     }
 
