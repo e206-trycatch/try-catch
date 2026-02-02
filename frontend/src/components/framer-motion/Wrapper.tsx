99bdb81a71d6e032a98c96a84ba1cd6204fa4db9
진채영 <devchaezerojj@gmail.com>
2026-02-02 12:31:28 +0900

    [S14P11E206-129] refactor(fe): ThemeSelectionPage CSS 깨짐 수정

--- frontend/src/components/framer-motion/Wrapper.tsx
+++ frontend/src/components/framer-motion/Wrapper.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 const Wrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.div
+      className="w-full"
       initial={{
         opacity: 0,
       }}
