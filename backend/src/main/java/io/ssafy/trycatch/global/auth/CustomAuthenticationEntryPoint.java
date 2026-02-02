a35a2fd33e4b293dfd1fdd7e836cceb3b4f1f24c
우주영 <dlsrtyex63@gmail.com>
2026-02-02 14:41:43 +0900

    [S14P11E206-513] fix(be): 만료 토큰으로 접근 시 401 응답 코드로 반환하는 로직 추가

---
+++ backend/src/main/java/io/ssafy/trycatch/global/auth/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,33 @@
+package io.ssafy.trycatch.global.auth;
+import io.ssafy.trycatch.global.common.ApiRespDto;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+        response.setContentType("application/json;charset=UTF-8");
+
+        // 기존 ApiResponse 형식에 맞춰서
+        String body = """
+                {
+                    "status": 401,
+                    "message": "인증이 필요합니다.",
+                    "data": null
+                }
+                """;
+
+        response.getWriter().write(body);
+    }
+}
