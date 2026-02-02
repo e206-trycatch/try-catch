8dd4e50021acd4be223baa8617c9a9af93ccf1d8
송은혁 <seh8145@naver.com>
2026-02-02 16:17:52 +0900

    [S14P11E206-326] feat(be) : 멀티모드 방 퇴장 구현

--- backend/src/main/java/io/ssafy/trycatch/domain/room/repository/RoomUserRepository.java
+++ backend/src/main/java/io/ssafy/trycatch/domain/room/repository/RoomUserRepository.java
@@ -4,8 +4,10 @@ import io.ssafy.trycatch.domain.room.entity.RoomUser;
 import io.ssafy.trycatch.global.common.TrueOrFalse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RoomUserRepository extends JpaRepository<RoomUser, Long> {
     Optional<RoomUser> findByRoomIdAndUserIdAndIsDeleted(Long roomId, Long userId, TrueOrFalse isDeleted);
+    List<RoomUser> findAllByRoomIdAndIsDeleted(Long roomId, TrueOrFalse isDeleted);
 }
